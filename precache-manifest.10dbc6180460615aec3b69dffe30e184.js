7fb1ebf11ed5001af21b14a346f2cd43713cb46f
AKAspanion <panditankit1995@gmail.com>
2019-05-11 22:26:52 +0530

    Updates

---
+++ precache-manifest.10dbc6180460615aec3b69dffe30e184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14acb4a142d2b3923270e0c6c64e05ea",
+    "url": "/react-redux-budget/index.html"
+  },
+  {
+    "revision": "a7ade08a74885bbd67ee",
+    "url": "/react-redux-budget/static/css/main.834c9416.chunk.css"
+  },
+  {
+    "revision": "6fa650cca4cddad74ce3",
+    "url": "/react-redux-budget/static/js/2.10d858db.chunk.js"
+  },
+  {
+    "revision": "a7ade08a74885bbd67ee",
+    "url": "/react-redux-budget/static/js/main.a6348c3a.chunk.js"
+  },
+  {
+    "revision": "f22230efea2a2904dd6f",
+    "url": "/react-redux-budget/static/js/runtime~main.d2470bf5.js"
+  },
+  {
+    "revision": "a418c2f808b5c20cf893b10807be006b",
+    "url": "/react-redux-budget/static/media/bg-banner.a418c2f8.jpg"
+  }
+]);
